8643e837df7cb3322908e72b5a93c9c38258a72b
Juan Otálora Alarcón <19978031+juanoa@users.noreply.github.com>
2024-08-31 20:18:03 +0200

    feat: open and close sub-trees

--- components/resources-tree/ResourcesTreeRow.tsx
+++ components/resources-tree/ResourcesTreeRow.tsx
@@ -1,7 +1,7 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Resource } from "@/modules/resources/domain/Resource";
-import { Folder, File } from "lucide-react";
+import { Folder, File, ChevronDown, ChevronUp } from "lucide-react";
 
 interface Props {
   resource: Resource;
@@ -9,14 +9,34 @@ interface Props {
 }
 
 export const ResourcesTreeRow: React.FC<Props> = ({ resource, level }) => {
-  const marginLeft = `${level * 20}px`;
+  const [isSubTreeOpen, setIsSubTreeOpen] = useState<boolean>(false);
 
   return (
     <>
-      <div className="rounded px-6 py-3 hover:bg-gray-50">
-        <div className="flex items-center gap-4" style={{ marginLeft }}>
+      <div
+        className={`group/icons ${Resource.isDirectory(resource) && `cursor-pointer`} rounded px-6 py-3 hover:bg-gray-50`}
+        onClick={() => setIsSubTreeOpen(!isSubTreeOpen)}
+      >
+        <div
+          className="flex items-center gap-4"
+          style={{ marginLeft: `${level * 20}px` }}
+        >
           {Resource.isDirectory(resource) ? (
-            <Folder size={18} />
+            <>
+              <Folder size={18} className="group-hover/icons:hidden" />
+              {isSubTreeOpen ? (
+                <ChevronUp
+                  size={18}
+                  className="hidden cursor-pointer group-hover/icons:block"
+                />
+              ) : (
+                <ChevronDown
+                  size={18}
+                  className="hidden cursor-pointer group-hover/icons:block"
+                  onClick={() => setIsSubTreeOpen(true)}
+                />
+              )}
+            </>
           ) : (
             <File size={18} />
           )}
@@ -24,6 +44,7 @@ export const ResourcesTreeRow: React.FC<Props> = ({ resource, level }) => {
         </div>
       </div>
       {Resource.isDirectory(resource) &&
+        isSubTreeOpen &&
         resource.children.map((child) => (
           <ResourcesTreeRow key={child.id} resource={child} level={level + 1} />
         ))}
