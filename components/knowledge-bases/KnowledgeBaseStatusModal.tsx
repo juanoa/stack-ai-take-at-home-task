4c890993347f14761a6392ce8073d595077ddfe2
Juan Otálora Alarcón <19978031+juanoa@users.noreply.github.com>
2024-09-01 16:51:08 +0200

    feat: allow users close the modal

--- components/knowledge-bases/KnowledgeBaseStatusModal.tsx
+++ components/knowledge-bases/KnowledgeBaseStatusModal.tsx
@@ -1,14 +1,23 @@
 import { KnowledgeBase } from "@/modules/knowledge-bases/domain/KnowledgeBase";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { KnowledgeBaseStatusResource } from "@/components/knowledge-bases/KnowledgeBaseStatusResource";
 import { KnowledgeBaseResource } from "@/modules/knowledge-bases/domain/KnowledgeBaseResource";
+import { X } from "lucide-react";
 
 interface Props {
   knowledgeBase?: KnowledgeBase;
 }
 export const KnowledgeBaseStatusModal: React.FC<Props> = ({ knowledgeBase }) => {
-  if (!knowledgeBase) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (knowledgeBase) {
+      setIsVisible(true);
+    }
+  }, [knowledgeBase]);
+
+  if (!isVisible || !knowledgeBase) {
     return null;
   }
 
@@ -16,9 +25,15 @@ export const KnowledgeBaseStatusModal: React.FC<Props> = ({ knowledgeBase }) => 
 
   return (
     <Card className="fixed bottom-10 left-10 w-[500px] shadow-lg">
-      <CardHeader>
-        <CardTitle>Indexing files</CardTitle>
-        <CardDescription>Indexing status in knowledge base</CardDescription>
+      <CardHeader className="flex flex-row justify-between">
+        <div>
+          <CardTitle>Indexing files</CardTitle>
+          <CardDescription>Indexing status in knowledge base</CardDescription>
+        </div>
+        <X
+          onClick={() => setIsVisible(false)}
+          className="cursor-pointer text-gray-600 hover:text-gray-900"
+        />
       </CardHeader>
       <CardContent>
         {resourcesSortedById.map((resource) => (
